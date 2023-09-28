3b89db8c307c68b630b8ca05dc921c929da9f400
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-09-28 22:50:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // your code here
+const name=document.getElementById("name");
+const year=document.getElementById("year");
+const h3=document.getElementById("url");
+const button=document.getElementById("button");
+button.addEventListener("click",()=>{
+	h3.innerText=h3.innerText+`?name=${name.value}&year=${year.value}`);
+})
